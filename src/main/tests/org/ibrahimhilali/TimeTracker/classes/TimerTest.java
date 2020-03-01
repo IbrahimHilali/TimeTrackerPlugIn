9731ce1262f69ae4482034ff9b21ade312ef0cc4
Ibrahim Hilali <ebrahelaly@gmail.com>
2020-03-01 18:29:07 +0100

    - added toString tests

--- src/main/tests/org/ibrahimhilali/TimeTracker/classes/TimerTest.java
+++ src/main/tests/org/ibrahimhilali/TimeTracker/classes/TimerTest.java
@@ -1,9 +1,19 @@
 package org.ibrahimhilali.TimeTracker.classes;
 
+import com.google.gson.Gson;
+import com.pholser.junit.quickcheck.From;
+import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import generators.Timers;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
 @RunWith(JUnitQuickcheck.class)
 public class TimerTest {
 
-
+    @Property(trials = 10)
+    public void testsToString(@From(Timers.class) Timer timer) {
+        assertEquals((new Gson()).toJson(timer), timer.toString());
+    }
 }
